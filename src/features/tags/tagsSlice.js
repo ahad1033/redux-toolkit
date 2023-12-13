b998e8a92e40f38cd293011e29df8700b4309067
ahad1033 <a.ahad2232@gmail.com>
2023-12-14 04:35:23 +0600

    tags slice added

---
+++ src/features/tags/tagsSlice.js
@@ -0,0 +1,39 @@
+import { getTags } from "./tagsAPIsAPI";
+
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+
+const initialState = {
+  tags: [],
+  isLoading: false,
+  isError: false,
+  error: "",
+};
+
+export const fetchTags = createAsyncThunk("videos/fetchTags", async () => {
+  const videos = await getTags();
+  return videos;
+});
+
+const tagsSlice = createSlice({
+  name: "videos",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTags.pending, (state) => {
+        state.isError = false;
+        state.isLoading = true;
+      })
+      .addCase(fetchTags.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.tags = action.payload;
+      })
+      .addCase(fetchTags.rejected, (state, action) => {
+        state.isLoading = false;
+        state.tags = [];
+        state.isError = true;
+        state.error = action.error?.message;
+      });
+  },
+});
+
+export default tagsSlice.reducer;
