a81fa83fe3591a50c6d3ca4afb6253dc4e7bb638
ahad1033 <a.ahad2232@gmail.com>
2023-12-14 03:58:03 +0600

    initials

--- src/features/videos/videosAPI.js
+++ src/features/videos/videosAPI.js
@@ -1,7 +1,6 @@
 import axios from "../../utils/axios";
 
 export const getVideos = async () => {
-    const response = await axios.get("/videos");
-
-    return response.data;
+  const response = await axios.get("/videos");
+  return response.data;
 };
